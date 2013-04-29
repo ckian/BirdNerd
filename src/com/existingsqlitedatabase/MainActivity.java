b5ab1645da0ca3e5818f8c25877df0d1e9527091
Christopher Kian <ckian@mail.csuchico.edu>
2013-04-28 19:47:47 -0700

    Query Button
    Testing the button which will return the spinner data, and run an sql
    query on it.

--- src/com/existingsqlitedatabase/MainActivity.java
+++ src/com/existingsqlitedatabase/MainActivity.java
@@ -8,27 +8,32 @@ import android.database.Cursor;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 
+	private Spinner colorSpin, locationSpin, shapeSpin, sizeSpin, billSpin, wingSpin, tailSpin, irisSpin;
+	private Button btn_query;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        Spinner colorSpin = (Spinner)findViewById(R.id.spinner_color);
-        Spinner locationSpin = (Spinner)findViewById(R.id.spinner_location);
-        Spinner shapeSpin = (Spinner)findViewById(R.id.spinner_shape);
-        Spinner sizeSpin = (Spinner)findViewById(R.id.spinner_size);
-        Spinner billSpin = (Spinner)findViewById(R.id.spinner_bill);
-        Spinner wingSpin = (Spinner)findViewById(R.id.spinner_wing);
-        Spinner tailSpin = (Spinner)findViewById(R.id.spinner_tail);
-        Spinner irisSpin = (Spinner)findViewById(R.id.spinner_iris);
+        colorSpin = (Spinner)findViewById(R.id.spinner_color);
+        locationSpin = (Spinner)findViewById(R.id.spinner_location);
+        shapeSpin = (Spinner)findViewById(R.id.spinner_shape);
+        sizeSpin = (Spinner)findViewById(R.id.spinner_size);
+        billSpin = (Spinner)findViewById(R.id.spinner_bill);
+        wingSpin = (Spinner)findViewById(R.id.spinner_wing);
+        tailSpin = (Spinner)findViewById(R.id.spinner_tail);
+        irisSpin = (Spinner)findViewById(R.id.spinner_iris);
         
         initDB();
         
@@ -40,8 +45,56 @@ public class MainActivity extends Activity {
         fillSpinner("wing", wingSpin);
         fillSpinner("tail", tailSpin);
         fillSpinner("iris", irisSpin);
+        
+        
+        btn_query = (Button) findViewById(R.id.btn_query);
+        
+//        final Button btnQuery = (Button) findViewById(R.id.btn_query);
+//
+//        btnQuery.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Perform action on click
+////            	String colorArg = colorSpin.getSelectedItem().toString();
+////            	getQuery(colorArg);
+//            }
+//        });        
     }
 
+    public void addListenerOnButton() {
+    	 
+    	colorSpin = (Spinner) findViewById(R.id.spinner_color);
+    	btn_query = (Button) findViewById(R.id.btn_query);
+     
+    	btn_query.setOnClickListener(new View.OnClickListener() {
+     
+    	  @Override
+    	  public void onClick(View v) {
+    		  
+    		  String h = String.valueOf(colorSpin.getSelectedItem());
+    		  
+    		  Utility.ShowMessageBox(MainActivity.this, h);
+//    	    Toast.makeText(MyAndroidAppActivity.this,
+//    		"OnClickListener : " + 
+//                    "\nSpinner 1 : "+ String.valueOf(colorSpin.getSelectedItem())
+//    			Toast.LENGTH_SHORT).show();
+    	}
+    	});
+    }
+    public void SearchBirds(View v)
+    {
+    	Spinner color = (Spinner)findViewById(R.id.spinner_color);
+    	
+    	int position = color.getSelectedItemPosition(); 
+//    	Cursor cursor = (Cursor) adapter.getItem(position);
+//    	String myText = cursor.getString(cursor.getColumnIndex("color_name"));
+    	
+    	
+    	
+        String c = color.getSelectedItem().toString();
+        Utility.ShowMessageBox(this, c + " , " + position);
+    }
+    
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
@@ -78,10 +131,15 @@ public class MainActivity extends Activity {
 //		// get reference to our spinner
 //		Spinner s = (Spinner) findViewById(R.id.spinner_wing);
 		spin.setAdapter(adapter);
-		
-
 	}
     
+//	public void getQuery(String s)
+//	{
+//		Utility.ShowMessageBox(this, s);
+//
+//	}
+	
+	
 //    public void LoadEmployee(View v)
 //    {
 //    	TestAdapter mDbHelper = new TestAdapter(this);         
